1645b7fc7323b763e4e7556f2abdd9308a8dc036
Walter <walterlaicollege@gmail.com>
2023-05-08 16:34:36 -0400

    a lot of changes

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 import numpy as np
+import openmm
+from openmm import *
+from openmm.app import *
+from openmm.unit import *
 
-def get_gradients(func, parameters, perturb=0.01):
+def get_gradients(func, parameters, perturb=0.0001):
     gradients = np.zeros(len(parameters))
     for param_number, parameter in enumerate(parameters):
         param_copy = parameters.copy()
@@ -26,3 +30,53 @@ def gradient_descent(func, starting_params, max_iter=100, step_len=0.1):
         counter += 1
 
     return params
+
+def coordinates_to_potential_sum(coordinates, func = lambda r:4*(r**-12-r**-6)):
+    potential_sum = 0
+    coordinates = coordinates.reshape((len(coordinates)//2,2))
+
+    for atom_i in range(len(coordinates)):
+        per_dimension_distances = coordinates[atom_i,:] - coordinates
+        interatomic_distances   = np.sqrt(np.sum(per_dimension_distances**2,axis=1))
+        interatomic_distances[np.isclose(interatomic_distances,0)] = 1
+        atom_i_potentials = func(interatomic_distances)
+        potential_sum += atom_i_potentials.sum()
+
+    return potential_sum/2 #its symmetric, we double counted
+
+class SimulationHelper():
+    def __init__(self, initial_coordinates):
+        self.natoms, self.ndims = initial_coordinates.shape
+        self.set_atoms(element="argon", atomic_number=18, mass=1*amu)
+        self.set_forces(sigma= 1*nanometer, epsilon=1*kilocalories_per_mole)
+        self.simulation = Simulation(     self.topology, self.system,
+                                      LangevinIntegrator(293.15*kelvin,1/picosecond,2*femtoseconds))
+        #self.simulation.context.setPositions(initial_coordinates*nanometer)
+
+    def set_atoms(self, element, atomic_number, mass):
+        self.system = System()
+        self.topology = Topology()
+        chain  = self.topology.addChain()
+        residue= self.topology.addResidue(element,chain)
+        element_object = Element.getByAtomicNumber(atomic_number)
+
+        for atom_index in range(self.natoms):
+            self.system.addParticle(mass)
+            self.topology.addAtom(element, element_object,residue)
+    
+    def set_forces(self, sigma, epsilon, charge=0.0):
+        self.force = openmm.NonbondedForce()
+        for atom_index in range(self.natoms):
+            self.force.addParticle(charge, sigma, epsilon)
+        self.system.addForce(self.force)
+
+    def potential_function(self, coordinates):
+        self.simulation.context.setPositions(coordinates*nanometer)
+        state = self.simulation.context.getState(getEnergy=True)
+        energy= state.getPotentialEnergy()/kilocalories_per_mole
+        return energy
+    
+    def potential_wrapper(self, coordinates):
+        coordinates = coordinates.reshape((len(coordinates)//2,2))
+        energy = self.potential_function(coordinates)
+        return energy
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import gradient_descent, get_gradients
+from functions import gradient_descent,get_gradients,coordinates_to_potential_sum,SimulationHelper
+
 
 class LinReg():
 
@@ -23,3 +24,29 @@ starting_params = [2,1]
 m,b = gradient_descent(regressor.chi_sq, starting_params, step_len=0.5)
 print(f'Expected m:{true_slope} b:{true_intercept}, Observed m:{m} b:{b}')
 
+coordinates = np.array([
+[-1,2,0],[-3,20,0],[-12,14,0]
+]
+, dtype=float)
+
+true_potential_sum = -2.8257776083489956e-06
+#print("Expected POtential",coordinates_to_potential_sum(coordinates, func = lambda r:4*(r**-12-r**-6)))
+
+simulator = SimulationHelper(coordinates.reshape(3,3))
+print("OpenMM Potential",simulator.potential_function(coordinates.reshape(3,3)))
+
+
+starting_params = rng.random(9)
+
+def potential_wrapper(coordinates):
+    return simulator.potential_function(coordinates.reshape((len(coordinates)//3,3)))
+    
+
+min_coordinates = gradient_descent(potential_wrapper, starting_params, step_len=0.01, max_iter=1000)
+min_coordinates = min_coordinates.reshape(len(min_coordinates)//3,3)
+starting_params = starting_params.reshape(len(starting_params)//3,3)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(*min_coordinates.T)
+plt.show()
