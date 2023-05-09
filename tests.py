0836278531322db99dfde38ad697a103a5f11fae
Walter <walterlaicollege@gmail.com>
2023-05-08 20:54:34 -0400

    added 2d plot

--- tests.py
+++ tests.py
@@ -32,10 +32,32 @@ coordinates = np.array([
 
 true_potential_sum = -2.8257776083489956e-06
 #print("Expected POtential",coordinates_to_potential_sum(coordinates, func = lambda r:4*(r**-12-r**-6)))
+print('Expected Potential', true_potential_sum)
 
 simulator = SimulationHelper(coordinates.reshape(3,3))
 print("OpenMM Potential",simulator.potential_function(coordinates.reshape(3,3)))
 
+starting_params = rng.random(6)
+steps, fxs = gradient_descent(coordinates_to_potential_sum, starting_params, step_len=0.01, max_iter=100, tolerance=0.01)
+steps = np.array(steps)
+nsteps, nparams = steps.shape
+steps = steps.reshape(nsteps, nparams//2, 2)
+fig, ax = plt.subplots(nrows=2)
+ax[0].set(xlim=(0,2),ylim=(-1,2), title='Gradient Descent of the Potential Energy Based on Coordinates')
+atoms = ax[0].scatter(steps[0,:,0], steps[0,:,1])
+ax[1].set(xlim=(0,len(fxs)), ylim=(0,max(fxs)))
+energies, = ax[1].plot([],[])
+ax[1].set(xlabel='Timestep',ylabel='Energy')
+
+def animate(i):
+    current_step = steps[i]
+    atoms.set_offsets(current_step[:,:2])
+    energies.set_data(np.arange(i),fxs[:i])
+    return atoms, energies
+
+anim = anm.FuncAnimation(fig, animate, frames = len(steps), blit=True)
+plt.show()
+print()
 
 starting_params = np.array([
     [0,0.01,0],
@@ -55,10 +77,14 @@ steps = steps.reshape(nsteps, nparams//3, 3)
 
 #https://stackoverflow.com/questions/41602588/how-to-create-3d-scatter-animations
 fig    = plt.figure()
-ax     = plt.axes(projection='3d')
-ax.view_init(elev=-119, azim=-146, roll=-20)
-ax.set(xlim=(-1,1), ylim=(-1,1), zlim=(-1,1))
-atoms, = ax.plot(*steps[0].T,linestyle='',marker='o')
+fig, axes = plt.subplots(nrows=2, ncols=1, subplot_kw=dict(projection="3d"))
+plt.subplots_adjust(wspace=0.5,hspace=0.5)
+axes[0].view_init(elev=-119, azim=-146, roll=-20)
+axes[0].set(xlim=(-1,1), ylim=(-1,1), zlim=(-1,1))
+atoms, = axes[0].plot(*steps[0].T,linestyle='',marker='o')
+energies, = axes[1].plot(range(len(steps)), fxs, 0)
+axes[1].ticklabel_format(axis='y',style='plain')
+axes[1].view_init(elev=101,azim=-92, roll=0)
 
 def animate(frame_number):
     coordinates = steps[frame_number]
@@ -70,3 +96,4 @@ anim = anm.FuncAnimation(fig, animate, len(steps), blit=True)
 plt.show()
 
 print()
+
